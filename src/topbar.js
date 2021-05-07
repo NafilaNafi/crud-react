9cbb8e91a434ac343ee82746cdddfa2296d72c54
NafilaNafi <Nafila@outlook.com>
2021-05-07 20:16:31 +0530

    change

--- src/topbar.js
+++ src/topbar.js
@@ -167,7 +167,8 @@ export default function Topbar(){
                             <Link class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg"/>
+                                <img class="img-profile rounded-circle" alt="profile" src="img/undraw_profile.svg"/>
+                                
                             </Link>
                             
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
